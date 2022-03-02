51f416450509d34fd301fcc56d9a89f7f6184be8
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-03-02 16:53:33 +0300

    create update event route

--- routes/events.js
+++ routes/events.js
@@ -14,7 +14,7 @@ router.get("/", (req, res, next) => {
 // Create new event
 router.post("/", (req, res, next) => {
   const { title, date, location, description, guestList } = req.body;
-  Event.create({ title, date, location, description, guestList: guestList.split(', ') })
+  Event.create({ title, date, location, description, guestList: guestList.split(' ') })
     .then(createdEvent => {
       res.status(201).json(createdEvent)
     })
@@ -30,7 +30,27 @@ router.get("/:id", (req, res, next) => {
     })
     .catch(err => next(err))
 });
-// Edit 
+
+// update a specific event
+router.put("/:id", (req, res, next) => {
+  const { id } = req.params
+  const { title, date, location, description, guestList } = req.body
+  Event.findByIdAndUpdate(id,
+    {
+      title,
+      date,
+      location,
+      description,
+      guestList: guestList.split(' ')
+    }, { new: true })
+    .then(updatedEvent => {
+      res.status(200).json(updatedEvent)
+    })
+    .catch(err => next(err))
+})
+
+
+
 // example: router.use("/auth", authRoutes)
 
 module.exports = router;
