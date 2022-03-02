b6b39075f9add0d13a63394a2042bf0f3c246db7
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-03-02 22:52:31 +0300

    implement edit event feature

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import EventDetails from './pages/EventDetails';
+import EventEdit from './pages/EventEdit';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/events/:id" element={<EventDetails />} />
+        <Route path="/events/:id/edit" element={<EventEdit />} />
       </Routes>
     </div>
   );
