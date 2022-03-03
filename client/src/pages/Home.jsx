d92e9159392bdb84634ebfcfd27f92d8ff77e5aa
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-03-03 21:03:00 +0300

    implement search feature

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,14 +5,15 @@ import EventCreateForm from '../components/EventCreateForm';
 import SearchBar from '../components/SearchBar';
 
 function Home() {
-
+    const [search, setSearch] = useState('')
     const [popup, setPopup] = useState(false)
+
 
     return (
         <>
             Home Page
-            <SearchBar />
-            <EventList />
+            <SearchBar setSearch={setSearch} search={search} />
+            <EventList setSearch={setSearch} search={search} />
             <button onClick={() => setPopup(true)}>Create a Hangout</button>
             <EventCreatePopup trigger={popup} setTrigger={setPopup}>
                 <h2>Create a Hangout</h2>
