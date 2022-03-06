a06ee9e1bba54bcef36120c60143dcfdbf63afaa
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-03-06 16:23:26 +0300

    initial implementation of Alan-AI

--- models/User.js
+++ models/User.js
@@ -2,11 +2,11 @@ const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
-    // username: {
-    //   type: String,
-    //   required: true,
-    //   unique: true
-    // },
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
     password: {
       type: String,
       required: true,
