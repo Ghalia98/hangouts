bec174d6de0ee467f80ee7dac2f4a4d9f3ad3014
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-03-07 18:47:58 +0300

    attempting to deploy

--- app.js
+++ app.js
@@ -29,4 +29,8 @@ app.use("/api/users", isAuthenticated, users);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+app.use((req, res) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/client/build/index.html");
+});
 module.exports = app;
